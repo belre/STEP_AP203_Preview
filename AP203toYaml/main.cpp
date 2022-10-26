3ffb4b6056febe2886e73bbfe242c7050ce21ade
Note PC <neopossam@gmail.com>
2022-10-26 13:31:28 +0900

    remove composite classes

--- AP203toYaml/main.cpp
+++ AP203toYaml/main.cpp
@@ -12,11 +12,6 @@
 
 #include <yaml-cpp/yaml.h>
 
-#include "StepComponent.h"
-#include "StepComposite.h"
-#include "StepDerivedNode.h"
-#include "StepNode.h"
-
 void PrintDebugMessage(int id, STEPattribute* attribute, std::stringstream& debug_log, int loop_count)
 {
 	for (int p = 0; p < loop_count; p++)
@@ -98,7 +93,7 @@ bool ExtractSelectInstanceId(InstMgr*& inst_mgr, SDAI_Select* select, BASE_TYPE 
 	return true;
 }
 
-void AddNode(InstMgr*& inst_mgr, StepComponent* base_component, SDAI_Application_instance* const& instance, std::stringstream& debug_log, YAML::Node& yaml_node, int loop_count, bool is_complex)
+void AddNode(InstMgr*& inst_mgr, std::vector<int>& stock_id, SDAI_Application_instance* const& instance, std::stringstream& debug_log, YAML::Node& yaml_node, int loop_count, bool is_complex)
 {
 	int id = instance->GetFileId();
 
@@ -106,26 +101,20 @@ void AddNode(InstMgr*& inst_mgr, StepComponent* base_component, SDAI_Application
 	{
 		yaml_node["sc_fileid"] = id;
 		yaml_node["sc_function"] = instance->EntityName();
+		stock_id.push_back(id);
 	}
 
 	auto instance_complex = dynamic_cast<const STEPcomplex*>(instance);
 	if(instance_complex != nullptr && !is_complex)  
 	{
 		yaml_node["is_complex"] = true;
-		std::cout << "#" << id << std::endl;
-		int cnt = 0;
-		for (auto iter = instance_complex->head; iter != nullptr; iter = iter->sc, ++cnt);
-		std::cout << "instance count:" << cnt << std::endl;
 
 		for (auto iter = instance_complex->head; iter != nullptr; iter = iter->sc)
 		{
 			auto inst = dynamic_cast<SDAI_Application_instance*>(iter);
 
-			StepComponent* child_node = new StepComposite(inst);
-			base_component->AddComponent(child_node, true);
-
 			YAML::Node yaml_child_node;
-			AddNode(inst_mgr, base_component, inst, debug_log, yaml_child_node, loop_count + 1, true);
+			AddNode(inst_mgr, stock_id, inst, debug_log, yaml_child_node, loop_count + 1, true);
 			yaml_node["complex_element"].push_back(yaml_child_node);
 		}
 
@@ -144,9 +133,6 @@ void AddNode(InstMgr*& inst_mgr, StepComponent* base_component, SDAI_Application
 
 		if (!attr_select && !attr_instance && !attr_aggr)
 		{
-			StepComponent* simple_node = new StepNode(instance);
-			base_component->AddComponent(simple_node, false);
-
 			auto name = attribute->Name();
 			yaml_node[name] = attribute->asStr();
 			continue;
@@ -154,9 +140,6 @@ void AddNode(InstMgr*& inst_mgr, StepComponent* base_component, SDAI_Application
 
 		if (attribute->IsDerived())
 		{
-			StepComponent* derived_node = new StepDerivedNode(instance);
-			base_component->AddComponent(derived_node, false);
-
 			yaml_node[attribute->Name()] = attribute->asStr();
 		}
 		else if(attr_aggr != nullptr)
@@ -179,11 +162,8 @@ void AddNode(InstMgr*& inst_mgr, StepComponent* base_component, SDAI_Application
 					auto inst = conv_node->node;
 					int inst_id = inst->GetFileId();
 
-					StepComponent* child_node = new StepComposite(inst);
-					base_component->AddComponent(child_node, false);
-
 					YAML::Node yaml_child_node;
-					AddNode(inst_mgr, child_node, inst, debug_log, yaml_child_node, loop_count + 1, false);
+					AddNode(inst_mgr, stock_id, inst, debug_log, yaml_child_node, loop_count + 1, false);
 
 					aggr_node.push_back(yaml_child_node);
 
@@ -263,21 +243,16 @@ void AddNode(InstMgr*& inst_mgr, StepComponent* base_component, SDAI_Application
 			}
 
 			auto select_instance = select_entity->GetApplication_instance();
-			StepComponent* child_node = new StepComposite(select_instance);
-			base_component->AddComponent(child_node, false);
 
 			YAML::Node yaml_child_node;
-			AddNode(inst_mgr, child_node, select_instance, debug_log, yaml_child_node, loop_count + 1, false);
+			AddNode(inst_mgr, stock_id, select_instance, debug_log, yaml_child_node, loop_count + 1, false);
 
 			yaml_node[attribute->Name()] = yaml_child_node;
 		}
 		else if(attr_instance != nullptr)
 		{
-			StepComponent* child_node = new StepComposite(attr_instance);
-			base_component->AddComponent(child_node, false);
-
 			YAML::Node yaml_child_node;
-			AddNode(inst_mgr, child_node, attr_instance, debug_log, yaml_child_node, loop_count + 1, false);
+			AddNode(inst_mgr, stock_id, attr_instance, debug_log, yaml_child_node, loop_count + 1, false);
 
 			yaml_node[attribute->Name()] = yaml_child_node;
 		}
@@ -307,23 +282,21 @@ int main( int argv, char** argc)
 
 	std::stringstream debug_log;
 	YAML::Node root_node;
-	auto root_component = new StepComposite();
+
+	std::vector<int> stock_id;
 
 	for(int i = 0 ; i < instance_list->InstanceCount(); i++ ) 
 	{
 		auto instance = instance_list->GetSTEPentity(i);
 		int file_id = instance->GetFileId();
 
-		if (!root_component->ContainFileId(file_id))
+		if(std::find(stock_id.begin(), stock_id.end(), file_id) == stock_id.end())
 		{
 			std::cout << "#" << file_id << "export" << std::endl;
 			debug_log << file_id << ":root(" << instance->EntityName() << ")" << std::endl;
 
-			StepComponent* base_node = new StepComposite(instance);
-			root_component->AddComponent(base_node, false);
-
 			YAML::Node step_node;
-			AddNode(instance_list, base_node, instance, debug_log, step_node, 1, false);
+			AddNode(instance_list, stock_id, instance, debug_log, step_node, 1, false);
 
 			std::stringstream ss_str;
 			ss_str << "#" << instance->GetFileId();
